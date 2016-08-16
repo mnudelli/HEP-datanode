7233c287ed9074fb128e8bd867f641aa6c666019
mnudelli <mnudelli@users.noreply.github.com>
2016-08-15 23:26:26 -0400

    fixed colon

--- manifests/services.pp
+++ manifests/services.pp
@@ -1,6 +1,6 @@
 # Hadoop Datanode Services
 class datanode::services {
-	service { 'hadoop-hdfs-datanode' 
+	service { 'hadoop-hdfs-datanode': 
 		ensure => 'running',
    		enable => true,
    		hasstatus => true,
