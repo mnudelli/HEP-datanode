8645f178949b7e90919d29b0d36f9569885e3ee4
mnudelli <mnudelli@users.noreply.github.com>
2016-08-20 00:29:07 -0400

    changed var

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,6 +3,6 @@
 class datanode::install {
 	package { 'osg-se-hadoop-datanode':
 		ensure => present,
-		require => File[$datanode::hadoop_dir],
+		require => File['/hadoop1', '/hadoop2','/hadoop3','/hadoop4','/hadoop5','/hadoop6','/hadoop7','/hadoop8','/hadoop9','/hadoop10','/hadoop11','/hadoop12'],
 	}
 }
